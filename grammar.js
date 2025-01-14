a0942246749bda748e473e7d12a9bf40ab78c0f3
Komo <afiqquraisyzulkarnain@gmail.com>
2025-01-14 17:59:23 +0800

    add more types

--- grammar.js
+++ grammar.js
@@ -8,25 +8,22 @@
 // @ts-check
 
 const PREC = {
-  LIST: -1,
-  DEFAULT: 1,
+  OR: 1,  /* or */
+  AND: 2, /* and */
+  COMP: 3,  /* < > <= >= ~= == */
+  BITOR: 4, /* | */
+  BITXOR: 5, /* ~ */
+  BITAND: 6, /* & */
+  BITSHIFT: 7, /* << >>> << */
+  CONCAT: 8, /* .. */
+  ARITH: 9,  /* + - */
+  FACTOR: 10, /* * /// // / %%% % */
+  UNARY: 11, /* not - # ~ & */
+  POW: 12,   /* ^ */
 
-  OR: 2,  /* or */
-  AND: 3, /* and */
-  COMP: 4,  /* < > <= >= ~= == */
-  BITOR: 6, /* | */
-  BITXOR: 7, /* ~ */
-  BITAND: 8, /* & */
-  BITSHIFT: 9, /* << >>> << */
-  CONCAT: 10, /* .. */
-  ARITH: 11,  /* + - */
-  FACTOR: 12, /* * /// // / %%% % */
-  UNARY: 13, /* not - # ~ & */
-  POW: 14,   /* ^ */
-
-  TGENERIC: 2,
-  TVARIANTS: 3,
-  TUNARY: 4,
+  TGENERIC: 1,
+  TVARIANTS: 2,
+  TUNARY: 3,
 };
 
 module.exports = grammar({
@@ -43,12 +40,15 @@ module.exports = grammar({
     $._start_preproc_expr,
     $._end_preproc_expr,
     $._content_preproc_inline,
-    $.string_opening,
-    $.string_ending,
-    $.string_content,
+    $._block_string_opening,
+    $._block_string_ending,
+    $._block_string_content,
     $._block_comment_opening,
     $._block_comment_ending,
     $._block_comment_content,
+    $._block_preproc_opening,
+    $._block_preproc_ending,
+    $._block_preproc_content,
   ],
 
   conflicts: $ => [
@@ -56,9 +56,6 @@ module.exports = grammar({
     [$._expression, $._variable, $._type],
   ],
 
-  inline: $ => [
-    $.comment,
-  ],
 
   rules: {
     chunk: $ => $._expression,
@@ -83,6 +80,11 @@ module.exports = grammar({
       $.preprocess_expr,
       $.binary_operation,
       $.unary_operation,
+    ),
+
+    preprocessor: $ => either(
+      ["##", alias(/[^\n\r]+/, $.lua_statement)],
+      [$._block_preproc_opening, alias($._block_preproc_content, $.lua_statement), $._block_preproc_ending],
     ),
 
     prefix_exp: $ => choice(
@@ -158,20 +160,54 @@ module.exports = grammar({
       );
     },
 
-    string: $ => seq(
-      field("opening", $.string_opening),
-      field("content", $.string_content),
-      field("ending", $.string_ending),
+    string: $ => choice($._single_quote_string, $._double_quote_string, $._block_string),
+
+    _block_string: $ => seq_field({
+      opening: alias($._block_string_opening, $.string_opening),
+      content: alias($._block_string_content, $.string_content),
+      ending: alias($._block_string_ending, $.string_ending),
+    }),
+
+    _single_quote_string: $ => seq_field({
+      opening: alias("'", $.string_opening),
+      content: optional(alias($._single_quote_string_content, $.string_content)),
+      ending: alias("'", $.string_ending),
+    }),
+
+    _double_quote_string: $ => seq_field({
+      opening: alias("\"", $.string_opening),
+      content: optional(alias($._double_quote_string_content, $.string_content)),
+      ending: alias("\"", $.string_ending),
+    }),
+
+    _single_quote_string_content: $ => repeat1(choice(
+      token.immediate(prec(1, /[^\n'\\]+/)),
+      $.escape_sequence
+    )),
+
+    _double_quote_string_content: $ => repeat1(choice(
+      token.immediate(prec(1, /[^\n"\\]+/)),
+      $.escape_sequence
+    )),
+
+    escape_sequence: _ => token.immediate(
+      seq('\\', choice(
+        /[\nabfnrtv\\"']/,
+        /z\s*/,
+        /[0-9]{1,3}/,
+        /x[0-9a-fA-F]{2}/,
+        /u\{[0-9a-fA-F]+\}/,
+      ))
     ),
 
-    table_constructor: $ => seq("{", optional($.field_list), "}"),
+    table_constructor: $ => seq("{", optional($._field_list), "}"),
 
-    field_list: $ => seq(
-      $.field_expression,
-      any_amount_of($._field_separator, $.field_expression),
+    _field_list: $ => seq(
+      $.table_field,
+      any_amount_of($._field_separator, $.table_field),
       optional($._field_separator)
     ),
-    field_expression: $ => choice(
+    table_field: $ => choice(
       $._field_pair,
       $._field_sugar_pair,
       field("value", $._expression),
@@ -236,7 +272,6 @@ module.exports = grammar({
       $._end_preproc_name,
     ),
 
-
     preprocess_expr: $ => seq(
       $._start_preproc_expr,
       alias(
@@ -246,37 +281,127 @@ module.exports = grammar({
       $._end_preproc_expr,
     ),
 
-    // idsuffixed      <-- (id DotIndex+)~>rfoldright
-    _id_suffixed: $ => prec(PREC.DEFAULT, seq($.identifier, atleast_amount_of(".", $.identifier))),
+    _id_suffixed: $ => prec(1, seq($.identifier, atleast_amount_of(".", $.identifier))),
 
     _type: $ => choice(
       field("type", $.identifier),
       field("type", $._id_suffixed),
-      $._type_unary,
-      $._type_variants,
-      $._type_generic,
+      $.type_unary,
+      $.type_variants,
+      $.type_generic,
+      $.type_record,
+      $.type_union,
+      $.type_enum,
+      $.type_array,
+      $.type_pointer,
+      $.type_variant,
     ),
-    _type_unary: $ => prec.left(PREC.TUNARY, seq(
-      field("suffix", choice($.type_pointer, $.type_optional, $.type_array)),
-      field("type", $._type),
+
+    type_unary: $ => prec.left(PREC.TUNARY, seq(
+      field("suffix", choice($.type_unary_pointer, $.type_unary_optional, $.type_unary_array)),
+      field("type", alias($._type, $.type)),
     )),
-    _type_variants: $ => prec.left(PREC.TVARIANTS, seq($._type, atleast_amount_of("|", $._type))),
-    _type_generic: $ => prec.left(PREC.TGENERIC, seq(
+
+    type_variants: $ => prec.left(PREC.TVARIANTS, seq($._type, atleast_amount_of("|", $._type))),
+
+    type_generic: $ => prec.left(PREC.TGENERIC, seq(
       field("type", $._type),
       either(
         ["(", field("args", optional($.type_args)), ")"],
         [$.table_constructor],
       ),
     )),
-    type_pointer: _ => "*",
-    type_optional: _ => "?",
-    type_array: $ => seq("[", optional($._expression), "]"),
 
-    type_args: $ => prec(PREC.LIST, choice(
+    type_unary_pointer: _ => "*",
+    type_unary_optional: _ => "?",
+    type_unary_array: $ => seq("[", optional($._expression), "]"),
+
+    type_args: $ => prec(-1, choice(
       seq($._type_arg, any_amount_of(",", $._type_arg)),
       $.table_constructor,
     )),
     _type_arg: $ => choice($._type, $._expression),
+
+    type_record: $ => seq(
+      "record",
+      "{",
+      alias($._type_field, $.type_record_field),
+      any_amount_of(
+        $._field_separator,
+        alias($._type_field, $.type_record_field),
+      ),
+      optional($._field_separator),
+      "}"
+    ),
+
+    type_union: $ => seq(
+      "union",
+      "{",
+      field("field", alias($._type_field, $.type_union_field)),
+      any_amount_of(
+        $._field_separator,
+        field("field", alias($._type_field, $.type_union_field)),
+      ),
+      optional($._field_separator),
+      "}",
+    ),
+
+    type_enum: $ => seq(
+      "enum",
+      may_seq(
+        "(",
+        field("enumtype", alias($._type, $.type)),
+        ")",
+      ),
+      "{",
+      field("field", alias($._enum_required_field, $.type_enum_field)),
+      any_amount_of(
+        $._field_separator,
+        field("field", alias($._enum_field, $.type_enum_field)),
+      ),
+      optional($._field_separator),
+      "}"
+    ),
+
+    type_array: $ => seq(
+      "array",
+      "(",
+      field("arraytype", alias($._type, $.type)),
+      may_seq(
+        ",",
+        alias("length", $._expression),
+      ),
+      ")"
+    ),
+
+    type_pointer: $ => seq(
+      "pointer",
+      "(",
+      field("pointertype", alias($._type, $.type)),
+      ")"
+    ),
+
+    type_variant: $ => seq(
+      "variant",
+      "(",
+      field("arg", $._variant_arg),
+      any_amount_of(
+        ",",
+        field("arg", $._variant_arg),
+      ),
+      ")"
+    ),
+
+    _type_field: $ => seq($.identifier, ":", $._type),
+
+    _enum_required_field: $ => seq($.identifier, "=", $._expression),
+    _enum_field: $ => seq($.identifier, may_seq("=", $._expression)),
+
+    _variant_arg: $ => prec(1, either(
+      [alias($._type, $.type)],
+      [$._parentheses_exp],
+      [$._expression],
+    )),
   },
 });
 
@@ -300,4 +425,8 @@ function either(...args) {
   let choices = [];
   args.forEach(arg => choices.push(seq(...arg)));
   return choice(...choices);
+}
+
+function seq_field(obj) {
+  return seq(...Object.keys(obj).map(k => field(k, obj[k])));
 }
--- src/scanner.c
+++ src/scanner.c
@@ -10,13 +10,17 @@ enum TokenType {
 
   PREPROC_INLINE_CONTENT,
 
-  STRING_OPENING,
-  STRING_ENDING,
-  STRING_CONTENT,
+  BLOCK_STRING_OPENING,
+  BLOCK_STRING_ENDING,
+  BLOCK_STRING_CONTENT,
 
   BLOCK_COMMENT_OPENING,
   BLOCK_COMMENT_ENDING,
   BLOCK_COMMENT_CONTENT,
+
+  BLOCK_PREPROC_OPENING,
+  BLOCK_PREPROC_ENDING,
+  BLOCK_PREPROC_CONTENT,
 };
 
 enum PreprocEndingType {
@@ -32,6 +36,12 @@ static inline uint64_t consume_length(TSLexer *lex, int32_t ch) {
     consume(lex);
   }
   return length;
+}
+static inline bool consume_check(TSLexer *lex, int32_t ch) {
+  bool valid = lex->lookahead == ch;
+  if (valid)
+    consume(lex);
+  return valid;
 }
 static inline void skip(TSLexer *lex) { lex->advance(lex, true); }
 
@@ -83,13 +93,10 @@ void tree_sitter_nelua_external_scanner_deserialize(void *payload,
 }
 
 static bool scan_preproc_inline_opening(TSLexer *lex, char opening) {
-  if (lex->lookahead == '#') {
+  if (consume_check(lex, '#') && lex->lookahead == opening) {
+    preproc_ending = opening == '[' ? ']' : '|';
     consume(lex);
-    if (lex->lookahead == opening) {
-      preproc_ending = opening == '[' ? ']' : '|';
-      consume(lex);
-      return true;
-    }
+    return true;
   }
 
   return false;
@@ -99,13 +106,12 @@ static bool scan_preproc_inline_ending(TSLexer *lex, char ending) {
   if (preproc_ending == 0)
     return false;
 
-  if (ending != 0 ? lex->lookahead == preproc_ending && preproc_ending == ending
-                  : lex->lookahead == ']' || lex->lookahead == '|') {
+  if ((ending != 0
+           ? consume_check(lex, preproc_ending) && preproc_ending == ending
+           : consume_check(lex, ']') || consume_check(lex, '|')) &&
+      lex->lookahead == '#') {
     consume(lex);
-    if (lex->lookahead == '#') {
-      consume(lex);
-      return true;
-    }
+    return true;
   }
 
   return false;
@@ -172,72 +178,18 @@ static bool scan_block_content(TSLexer *lex) {
 }
 
 static bool scan_block_comment_opening(TSLexer *lex) {
-  if (lex->lookahead != '-')
-    return false;
-  consume(lex);
-
-  if (lex->lookahead != '-')
-    return false;
-  consume(lex);
-
-  return scan_block_opening(lex);
-}
-
-static bool scan_string_opening(TSLexer *lex) {
-  if (lex->lookahead == '[') {
-    return scan_block_opening(lex);
-  }
-
-  if (lex->lookahead == '\'' || lex->lookahead == '"') {
-    string_ending = lex->lookahead;
-    consume(lex);
+  if (consume_check(lex, '-') && consume_check(lex, '-') && scan_block_opening(lex)) {
     return true;
   }
+  
   return false;
 }
 
-static bool scan_string_ending(TSLexer *lex) {
-  if (lex->lookahead == ']')
-    return scan_block_ending(lex);
-
-  if (string_ending != 0 || lex->lookahead == string_ending) {
-    consume(lex);
+static bool scan_block_preproc_opening(TSLexer* lex) {
+  if (consume_check(lex, '#') && consume_check(lex, '#') && scan_block_opening(lex)) {
     return true;
   }
-
-  return false;
-}
-
-static bool scan_string_content(TSLexer *lex) {
-  if (string_ending == 0)
-    return scan_block_content(lex);
-
-  while (lex->lookahead != 0) {
-    if (lex->lookahead == string_ending) {
-      return true;
-    }
-
-    if (lex->lookahead == '\\') {
-      consume(lex);
-      if (lex->lookahead == 'z') {
-        consume(lex);
-        while (iswspace(lex->lookahead)) {
-          consume(lex);
-        }
-        continue;
-      } else if (lex->lookahead == '\n') {
-        consume(lex);
-        continue;
-      }
-    }
-
-    if (lex->lookahead == '\n') {
-      return false;
-    }
-
-    consume(lex);
-  }
-
+  
   return false;
 }
 
@@ -257,9 +209,9 @@ bool tree_sitter_nelua_external_scanner_scan(void *payload, TSLexer *lex,
     return true;
   }
 
-  if (valid_symbols[STRING_ENDING] && scan_string_ending(lex)) {
+  if (valid_symbols[BLOCK_STRING_ENDING] && scan_block_ending(lex)) {
     reset_state();
-    lex->result_symbol = STRING_ENDING;
+    lex->result_symbol = BLOCK_STRING_ENDING;
     return true;
   }
 
@@ -269,19 +221,30 @@ bool tree_sitter_nelua_external_scanner_scan(void *payload, TSLexer *lex,
     return true;
   }
 
+  if (valid_symbols[BLOCK_PREPROC_ENDING] && scan_block_ending(lex)) {
+    reset_state();
+    lex->result_symbol = BLOCK_PREPROC_ENDING;
+    return true;
+  }
+
   if (valid_symbols[PREPROC_INLINE_CONTENT] &&
       scan_preproc_inline_content(lex)) {
     lex->result_symbol = PREPROC_INLINE_CONTENT;
     return true;
   }
 
-  if (valid_symbols[STRING_CONTENT] && scan_string_content(lex)) {
-    lex->result_symbol = STRING_CONTENT;
+  if (valid_symbols[BLOCK_STRING_CONTENT] && scan_block_content(lex)) {
+    lex->result_symbol = BLOCK_STRING_CONTENT;
     return true;
   }
 
   if (valid_symbols[BLOCK_COMMENT_CONTENT] && scan_block_content(lex)) {
     lex->result_symbol = BLOCK_COMMENT_CONTENT;
+    return true;
+  }
+
+  if (valid_symbols[BLOCK_PREPROC_CONTENT] && scan_block_content(lex)) {
+    lex->result_symbol = BLOCK_PREPROC_CONTENT;
     return true;
   }
 
@@ -299,13 +262,18 @@ bool tree_sitter_nelua_external_scanner_scan(void *payload, TSLexer *lex,
     return true;
   }
 
-  if (valid_symbols[STRING_OPENING] && scan_string_opening(lex)) {
-    lex->result_symbol = STRING_OPENING;
+  if (valid_symbols[BLOCK_STRING_OPENING] && scan_block_opening(lex)) {
+    lex->result_symbol = BLOCK_STRING_OPENING;
     return true;
   }
 
   if (valid_symbols[BLOCK_COMMENT_OPENING] && scan_block_comment_opening(lex)) {
     lex->result_symbol = BLOCK_COMMENT_OPENING;
+    return true;
+  }
+
+  if (valid_symbols[BLOCK_PREPROC_OPENING] && scan_block_preproc_opening(lex)) {
+    lex->result_symbol = BLOCK_PREPROC_OPENING;
     return true;
   }
 
